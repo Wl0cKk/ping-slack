cd5abe727e0967c41aa48f12c323f136e0a9e326
Wl0cKk <kandraciuk404@gmail.com>
2025-01-12 17:24:38 +0300

    conf

--- bot.rb
+++ bot.rb
@@ -8,13 +8,13 @@ Slack.configure { |config| config.token = TOKEN }
 client = Slack::Web::Client.new
 notifier = INotify::Notifier.new
 
-watched_file = './README.txt'
+watched_file = '/watched_file'
 last_message_ts = nil
 
 notifier.watch(watched_file, :modify) do
     begin
-        client.chat_delete(channel: '#pingpong', ts: last_message_ts) if last_message_ts
-        response = client.chat_postMessage(channel: '#pingpong', text: "ping - #{SecureRandom.uuid}")
+        client.chat_delete(channel: CHANNEL, ts: last_message_ts) if last_message_ts
+        response = client.chat_postMessage(channel: CHANNEL, text: "ping - #{SecureRandom.uuid}")
         last_message_ts = response['ts']
     rescue Slack::Web::Api::Error => e
         puts "Error sending/deleting message: #{e.message}"
